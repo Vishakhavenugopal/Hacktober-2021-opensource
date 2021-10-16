f9fe271f83a85271eda045886a7f77a15da68926
Geek <73483969+19dudedemo@users.noreply.github.com>
2021-10-16 20:28:23 +0530

    Add files via upload

---
+++ redux_todolist/src/actions/index.js
@@ -0,0 +1,22 @@
+export const addItem = (data) => {
+  return {
+    type: "ADD_ITEM",
+    payload: {
+      id: new Date().getTime().toString(),
+      data: data,
+    },
+  };
+};
+
+export const deleteItem = (id) => {
+  return {
+    type: "DELETE_ITEM",
+    id,
+  };
+};
+
+export const removeAll = () => {
+  return {
+    type: "REMOVE_ALL",
+  };
+};
